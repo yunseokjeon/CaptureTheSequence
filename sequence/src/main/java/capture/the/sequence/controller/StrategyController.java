875c1457ec4609ff3718ffe87857994a22863787
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2021-12-16 12:46:29 +0900

    getPossessionItems

--- sequence/src/main/java/capture/the/sequence/controller/StrategyController.java
+++ sequence/src/main/java/capture/the/sequence/controller/StrategyController.java
@@ -4,11 +4,13 @@ import capture.the.sequence.dto.StrategyDTO;
 import capture.the.sequence.service.StrategyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RestController
@@ -19,7 +21,12 @@ public class StrategyController {
     private final StrategyService strategyService;
 
     @GetMapping("/getStrategies")
-    public List<StrategyDTO> getStrategies(){
+    public List<StrategyDTO> getStrategies() {
         return strategyService.getStrategies();
+    }
+
+    @GetMapping("/getPossessionItems")
+    public Set<String> getPossessionItems(@AuthenticationPrincipal String userId) {
+        return strategyService.getPossessionItems(userId);
     }
 }
--- sequence/src/main/java/capture/the/sequence/service/StrategyService.java
+++ sequence/src/main/java/capture/the/sequence/service/StrategyService.java
@@ -2,21 +2,42 @@ package capture.the.sequence.service;
 
 import capture.the.sequence.dto.Strategy;
 import capture.the.sequence.dto.StrategyDTO;
+import capture.the.sequence.model.PriceEntity;
+import capture.the.sequence.model.UserEntity;
+import capture.the.sequence.persistence.PriceRepository;
+import capture.the.sequence.persistence.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class StrategyService {
 
+    private final UserRepository userRepository;
+    private final PriceRepository priceRepository;
+
     public List<StrategyDTO> getStrategies() {
         List<StrategyDTO> strategies = new ArrayList<>();
         strategies.add(new StrategyDTO(Strategy.PYRAMIDING, "Jesse Livermore의 거래 전략"));
         return strategies;
+    }
+
+    public Set<String> getPossessionItems(String userId) {
+        Set<String> itemSet = new HashSet<>();
+        UserEntity user = userRepository.getById(userId);
+        user.getPriceEntityList().clear();
+        List<PriceEntity> priceEntities = priceRepository.findAllByUser(user);
+        user.setPriceEntityList(priceEntities);
+        for (PriceEntity priceEntity : user.getPriceEntityList()) {
+            itemSet.add(priceEntity.getItemName());
+        }
+        return itemSet;
     }
 }
