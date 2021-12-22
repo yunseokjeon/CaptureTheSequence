e2b850650b15ca2cccc2e17565151f809ec9ff6f
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2021-12-22 15:06:28 +0900

    simulation graph

--- react-sequence/src/containers/SimulationView.js
+++ react-sequence/src/containers/SimulationView.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {getStrategies} from "../service/ApiService";
 import {getPossessionItems} from "../service/ApiService";
 import {calculatePyramiding} from "../service/ApiService";
+import {ResponsiveLine} from '@nivo/line';
+import "./SimulationView.css";
+
 
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -9,6 +12,27 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import {Typography} from "@mui/material";
+
+const refineDataForOptimalKelly = (kellyXAxis, kellyYAxis) => {
+    let arr = [];
+    for (let i = 0; i < kellyXAxis.length; i++) {
+        let obj = {
+            "x": kellyXAxis[i],
+            "y": kellyYAxis[i]
+        };
+        arr.push(obj);
+    }
+
+    let obj = {
+        "id": "Optimal Kelly",
+        "color": "hsl(293, 70%, 50%)",
+        "data": arr
+    };
+
+    let result = [];
+    result.push(obj);
+    return result;
+}
 
 const SimulationView = () => {
 
@@ -21,6 +45,7 @@ const SimulationView = () => {
     const [kellyRatio, setKellyRatio] = React.useState(0);
     const [kellyXAxis, setKellyXAxis] = React.useState([]);
     const [kellyYAxis, setKellyYAxis] = React.useState([]);
+    const [optimalData, setOptimalData] = React.useState([]);
 
     React.useEffect(() => {
         getStrategies().then((res) => {
@@ -60,6 +85,8 @@ const SimulationView = () => {
                 setKellyRatio(res.kellyRatio);
                 setKellyXAxis(res.kellyXAxis);
                 setKellyYAxis(res.kellyYAxis);
+                let result = refineDataForOptimalKelly(res.kellyXAxis, res.kellyYAxis);
+                setOptimalData(result);
             })
         }
 
@@ -113,13 +140,69 @@ const SimulationView = () => {
 
             <button onClick={calculateKelly}>켈리 비율 계산</button>
 
-
-            <div>
-                {JSON.stringify(kellyRatio)}
-                {JSON.stringify(capitalGrowth)}
-                {JSON.stringify(kellyXAxis)}
-                {JSON.stringify(kellyYAxis)}
+            <div className={"optimalDiv"}>
+                <ResponsiveLine
+                    data={optimalData}
+                    margin={{top: 50, right: 110, bottom: 50, left: 60}}
+                    xScale={{type: 'point'}}
+                    yScale={{type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false}}
+                    yFormat=" >-.2f"
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                        orient: 'bottom',
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'transportation',
+                        legendOffset: 36,
+                        legendPosition: 'middle'
+                    }}
+                    axisLeft={{
+                        orient: 'left',
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'count',
+                        legendOffset: -40,
+                        legendPosition: 'middle'
+                    }}
+                    pointSize={10}
+                    pointColor={{theme: 'background'}}
+                    pointBorderWidth={2}
+                    pointBorderColor={{from: 'serieColor'}}
+                    pointLabelYOffset={-12}
+                    useMesh={true}
+                    legends={[
+                        {
+                            anchor: 'bottom-right',
+                            direction: 'column',
+                            justify: false,
+                            translateX: 100,
+                            translateY: 0,
+                            itemsSpacing: 0,
+                            itemDirection: 'left-to-right',
+                            itemWidth: 80,
+                            itemHeight: 20,
+                            itemOpacity: 0.75,
+                            symbolSize: 12,
+                            symbolShape: 'circle',
+                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemBackground: 'rgba(0, 0, 0, .03)',
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                />
             </div>
+
+            <div>최적 베팅 비율은 {kellyRatio} 입니다.</div>
         </div>
 
 
