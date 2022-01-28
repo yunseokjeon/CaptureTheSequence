c32e6deb649f707d62c501a929a39056626f845c
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2022-01-28 10:43:37 +0900

    config update

--- react-sequence/src/service/app-config.js
+++ react-sequence/src/service/app-config.js
@@ -5,6 +5,8 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
     // backendHost = "http://localhost:8080";
     backendHost = "http://52.78.42.7:8080";
+}else{
+    backendHost = "http://52.78.42.7:8080";
 }
 
 export const API_BASE_URL = `${backendHost}`;
