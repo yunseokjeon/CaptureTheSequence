d11cf8d4c0cddfa2bc5e1db88be98581df49b855
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2022-02-05 15:57:22 +0900

    hide @SpringBootTest because of gradle build

--- sequence/src/test/java/capture/the/sequence/SequenceApplicationTests.java
+++ sequence/src/test/java/capture/the/sequence/SequenceApplicationTests.java
@@ -3,7 +3,7 @@ package capture.the.sequence;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SequenceApplicationTests {
 
 	@Test
